da92bb2e1be3d9644be664b2cfbcc60633661575
VamC <vwithv1602@gmail.com>
2018-02-10 21:13:00 +0530

    Placing try catch in lead_conversion to prevent error due to customers without address

--- erpnext_uyn_customizations/__init__.py
+++ erpnext_uyn_customizations/__init__.py
@@ -10,10 +10,15 @@ from vlog import vwrite
 @frappe.whitelist()
 def lead_conversion(so,method):
     so = so.__dict__
-    customer = so.get("customer")
-    address_res = frappe.get_doc("Address", {"address_title": customer})
-    address = address_res.__dict__
-    email = address.get("email_id")
-    mobile = address.get("phone")
-    from lead import check_if_lead_exists
-    check_if_lead_exists(mobile,email)
+    try:    
+        customer = so.get("customer")
+        address_res = frappe.get_doc("Address", {"address_title": customer})
+        address = address_res.__dict__
+        email = address.get("email_id")
+        mobile = address.get("phone")
+        from lead import check_if_lead_exists
+        check_if_lead_exists(mobile,email)
+    except Exception, e:
+        vwrite("Exception occurred in lead_conversion")
+        vwrite(so)
+        vwrite(e.message)
