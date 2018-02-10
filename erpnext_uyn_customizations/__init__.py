ee398f2caecfedf4d266193628ae92a045745536
VamC <vwithv1602@gmail.com>
2018-02-10 21:42:41 +0530

    Removing the exception message

--- erpnext_uyn_customizations/__init__.py
+++ erpnext_uyn_customizations/__init__.py
@@ -10,14 +10,19 @@ from vlog import vwrite
 @frappe.whitelist()
 def lead_conversion(so,method):
     so = so.__dict__
-    try:    
+    try:
         customer = so.get("customer")
-        address_res = frappe.get_doc("Address", {"address_title": customer})
-        address = address_res.__dict__
-        email = address.get("email_id")
-        mobile = address.get("phone")
-        from lead import check_if_lead_exists
-        check_if_lead_exists(mobile,email)
+        address_query = """ select * from tabAddress where address_title='%s' order by creation desc
+        """ % customer
+        address_result = frappe.db.sql(address_query, as_dict=1)
+        if len(address_result)>0: 
+            address = address_result[0]
+            email = address.get("email_id")
+            mobile = address.get("phone")
+            from lead import check_if_lead_exists
+            check_if_lead_exists(mobile,email)
+        else:
+            vwrite("No address found for customer: %s" % customer)
     except Exception, e:
         vwrite("Exception occurred in lead_conversion")
         vwrite(so)
