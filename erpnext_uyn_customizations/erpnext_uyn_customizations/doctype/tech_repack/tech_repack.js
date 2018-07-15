6f793c4e089f01581b9f2bd4316d6ece223b73e8
Vamsi Krishna <vwithv1602@gmail.com>
2018-07-15 11:47:30 +0000

    tech repack - make repack functionality

--- erpnext_uyn_customizations/erpnext_uyn_customizations/doctype/tech_repack/tech_repack.js
+++ erpnext_uyn_customizations/erpnext_uyn_customizations/doctype/tech_repack/tech_repack.js
@@ -5,7 +5,16 @@ frappe.provide("erpnext.stock");
 
 frappe.ui.form.on('Tech Repack', {
 	refresh: function(frm) {
-
+		frm.add_custom_button(__("Repack"), function() {
+			// var excise = frappe.model.make_new_doc_and_get_name('Journal Entry');
+			// excise = locals['Journal Entry'][excise];
+			// excise.voucher_type = 'Excise Entry';
+			// frappe.set_route('Form', 'Stock Entry', "New Stock Entry");
+			frappe.model.open_mapped_doc({
+				method: "erpnext_uyn_customizations.erpnext_uyn_customizations.doctype.tech_repack.tech_repack.make_repack",
+				frm: frm
+			})
+		}, __("Make"));
 	},
 	barcode: function(frm){
 		if(frm.doc.barcode){
@@ -47,6 +56,30 @@ switch(user){
 		default_source_warehouse = default_target_warehouse = "Ready To Ship - Uyn";
 	break;
 }
+frappe.ui.form.on('Tech Repack Items', {
+	barcode: function(doc,cdt,cdn){
+		var barcode_val = frappe.model.get_value(cdt, cdn, "barcode");
+		if(barcode_val){
+			frappe.call({
+				method: "erpnext.stock.get_item_details.get_item_code",
+				args: {"barcode": barcode_val },
+				callback: function(r) {
+					if (!r.exe){
+						frappe.model.set_value(cdt, cdn, "item", r.message);
+						if(typeof r.message=='string'){
+							frappe.model.set_value(cdt, cdn, "item", r.message);
+						}else{
+							frappe.model.set_value(cdt, cdn, "item", r.message.item_code);
+							setTimeout(function(){
+								frappe.model.set_value(cdt, cdn, "warehouse", r.message.warehouse);
+							},600);
+						} 
+					}
+				}
+			});
+		}
+	}
+});
 frappe.ui.form.on('Tech Repack Taken Items', {
 	item_code: function(doc,cdt,cdn){
 		frappe.model.set_value(cdt, cdn, "source_warehouse", default_source_warehouse);
@@ -57,3 +90,4 @@ frappe.ui.form.on('Tech Repack Given Items', {
 		frappe.model.set_value(cdt, cdn, "target_warehouse", default_target_warehouse);
 	},
 });	
+
--- erpnext_uyn_customizations/erpnext_uyn_customizations/doctype/tech_repack/tech_repack.py
+++ erpnext_uyn_customizations/erpnext_uyn_customizations/doctype/tech_repack/tech_repack.py
@@ -5,6 +5,114 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
+from frappe.utils import cstr, flt, getdate, comma_and, cint
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.utils import get_fetch_values
+from frappe.contacts.doctype.address.address import get_company_address
+from erpnext.vlog import vwrite
 class TechRepack(Document):
 	pass
+@frappe.whitelist()
+def make_repack(source_name, target_doc=None, ignore_permissions=False):
+	def postprocess(source, target):
+		set_missing_values(source, target)
+		#Get the advance paid Journal Entries in Sales Invoice Advance
+		# target.set_advances()
+		target.purpose = "Repack"
+
+	def set_missing_values(source, target):
+		target.is_pos = 0
+		target.ignore_pricing_rule = 1
+		target.flags.ignore_permissions = True
+		target.run_method("set_missing_values")
+		# target.run_method("set_po_nos")
+		# target.run_method("calculate_taxes_and_totals")
+
+		# set company address
+		# target.update(get_company_address(target.company))
+		# if target.company_address:
+		# 	target.update(get_fetch_values("Sales Invoice", 'company_address', target.company_address))
+
+	def update_item(source, target, source_parent):
+		# target.amount = flt(source.amount) - flt(source.billed_amt)
+		# target.base_amount = target.amount * flt(source_parent.conversion_rate)
+		# target.qty = target.amount / flt(source.rate) if (source.rate and source.billed_amt) else source.qty
+
+		# item = frappe.db.get_value("Item", target.item_code, ["item_group", "selling_cost_center"], as_dict=1)
+		# target.cost_center = frappe.db.get_value("Project", source_parent.project, "cost_center") \
+		# 	or item.selling_cost_center \
+		# 	or frappe.db.get_value("Item Group", item.item_group, "default_cost_center")
+		target.barcode=source.barcode
+		target.s_warehouse=source.warehouse
+		target.item_code=source.item
+		target.serial_no=source.barcode
+	def update_taken_item(source, target, source_parent):
+		# target.amount = flt(source.amount) - flt(source.billed_amt)
+		# target.base_amount = target.amount * flt(source_parent.conversion_rate)
+		# target.qty = target.amount / flt(source.rate) if (source.rate and source.billed_amt) else source.qty
+
+		# item = frappe.db.get_value("Item", target.item_code, ["item_group", "selling_cost_center"], as_dict=1)
+		# target.cost_center = frappe.db.get_value("Project", source_parent.project, "cost_center") \
+		# 	or item.selling_cost_center \
+		# 	or frappe.db.get_value("Item Group", item.item_group, "default_cost_center")
+		target.s_warehouse=source.source_warehouse
+		target.item_code=source.item_code
+		target.qty=source.qty
+		target.uom='Nos'
+		target.conversion_factor='1'
+	def update_given_item(source, target, source_parent):
+		# target.amount = flt(source.amount) - flt(source.billed_amt)
+		# target.base_amount = target.amount * flt(source_parent.conversion_rate)
+		# target.qty = target.amount / flt(source.rate) if (source.rate and source.billed_amt) else source.qty
+
+		# item = frappe.db.get_value("Item", target.item_code, ["item_group", "selling_cost_center"], as_dict=1)
+		# target.cost_center = frappe.db.get_value("Project", source_parent.project, "cost_center") \
+		# 	or item.selling_cost_center \
+		# 	or frappe.db.get_value("Item Group", item.item_group, "default_cost_center")
+		target.t_warehouse=source.target_warehouse
+		target.item_code=source.item_code
+		target.qty=source.qty
+		target.uom='Nos'
+		target.conversion_factor='1'
+
+	doclist = get_mapped_doc("Tech Repack", source_name, {
+		"Tech Repack": {
+			"doctype": "Stock Entry",
+			"field_map": {
+				"item_code": "item_code"
+			},
+			"validation": {
+				"docstatus": ["=", 1]
+			}
+		},
+		# "Tech Repack": {
+		# 	"doctype": "Stock Entry Detail",
+		# 	"field_map": {
+		# 		# "name": "so_detail",
+		# 		# "item_code": "item_code",
+		# 	},
+		# 	"postprocess": update_item,
+		# 	# "condition": lambda doc: doc.qty and (doc.base_amount==0 or abs(doc.billed_amt) < abs(doc.amount))
+		# },
+		"Tech Repack Taken Items": {
+			"doctype": "Stock Entry Detail",
+			"field_map": {
+				# "name": "so_detail",
+				# "item_code": "item_code",
+			},
+			"postprocess": update_taken_item,
+			# "condition": lambda doc: doc.qty and (doc.base_amount==0 or abs(doc.billed_amt) < abs(doc.amount))
+		},
+		"Tech Repack Given Items": {
+			"doctype": "Stock Entry Detail",
+			"field_map": {
+				# "name": "so_detail",
+				"item_code": "item_code",
+			},
+			"postprocess": update_given_item,
+			# "condition": lambda doc: doc.qty and (doc.base_amount==0 or abs(doc.billed_amt) < abs(doc.amount))
+		}
+	}, target_doc, postprocess, ignore_permissions=ignore_permissions)
+
+	return doclist
+
