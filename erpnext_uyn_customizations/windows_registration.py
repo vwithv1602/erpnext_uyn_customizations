963d8fa38be1f062ccbc133640cf1909ace6154c
khopadi <it@usedyetnew.com>
2019-06-18 15:38:11 +0530

    Testing changes

--- erpnext_uyn_customizations/windows_registration.py
+++ erpnext_uyn_customizations/windows_registration.py
@@ -279,7 +279,6 @@ def registration(info):
     customer_address_doc.email_id = info['email_id']
     customer_address_doc.phone = info['phone']
     customer_address_doc.save(ignore_permissions=True)
-    customer_address_doc.submit()
     vwrite(customer_address_doc.email_id)
     
 def validate(info):
