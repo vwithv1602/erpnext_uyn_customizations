c91bc9da0a5b18490aafc8cbee19ea8256cf0519
khopadi <it@usedyetnew.com>
2019-06-18 15:00:53 +0530

    Testing changes

--- erpnext_uyn_customizations/windows_registration.py
+++ erpnext_uyn_customizations/windows_registration.py
@@ -272,13 +272,13 @@ def registration(info):
             'status':False,
             'error': "Address not found"
         }
-    frappe.session.user = "it"
-    client = FrappeClient("http://35.154.240.105","it@usedyetnew.com","thisisit1#")
-    vwrite(client)
-    customer_address_doc = client.get_doc("Address", customer_address_doc_name)
-    customer_address_doc["email_id"] = info['email_id']
-    customer_address_doc["phone"] = info['phone']
-    client.update(customer_address_doc)
+    #frappe.session.user = "it"
+    #client = FrappeClient("http://35.154.240.105","it@usedyetnew.com","thisisit1#")
+    #vwrite(client)
+    customer_address_doc = frappe.get_doc("Address", customer_address_doc_name)
+    customer_address_doc.email_id = info['email_id']
+    customer_address_doc.phone = info['phone']
+    customer_address_doc.save(ignore_permissions=True)
     
 def validate(info):
     key_to_function_map = {
