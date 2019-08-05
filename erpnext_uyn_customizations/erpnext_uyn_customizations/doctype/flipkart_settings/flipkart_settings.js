2ed83f14ec2678e1ec1ff237f33017ec060a1506
khopadi <it@usedyetnew.com>
2019-08-05 15:11:38 +0530

    Reverting changes

--- erpnext_uyn_customizations/erpnext_uyn_customizations/doctype/flipkart_settings/flipkart_settings.js
+++ erpnext_uyn_customizations/erpnext_uyn_customizations/doctype/flipkart_settings/flipkart_settings.js
@@ -5,7 +5,6 @@ frappe.ui.form.on('Flipkart Settings', {
 	refresh: function(frm) {
 		if(!frm.doc.__islocal && frm.doc.enable_flipkart === 1){
             frm.add_custom_button(__('Sync Flipkart'), function() {
-                frappe.msgprint("Reached here")
                 frappe.call({
                     method:"erpnext_uyn_customizations.flipkart_api.sync_flipkart",
                 })
