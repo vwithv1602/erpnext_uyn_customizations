e14de865709d2e73cbef755b71ec493fcde96b5d
khopadi <it@usedyetnew.com>
2019-08-06 12:34:04 +0530

    changes in the windows registration API

--- erpnext_uyn_customizations/windows_registration.py
+++ erpnext_uyn_customizations/windows_registration.py
@@ -17,7 +17,7 @@ from erpnext_ebay.vlog import vwrite
 def registration(info):
     info = ast.literal_eval(info)
 
-    if info["order_id"]:
+    if info["order_id"] and info['order_id'] != "":
         # Getting the sales order id for the order id provided.
         amazon_sales_order_id = frappe.db.get_value("Sales Order", {'amazon_order_id':info["order_id"]}, 'name')
         flipkart_sales_order_id = frappe.db.get_value("Sales Order", {'flipkart_order_id':info["order_id"]}, 'name')
@@ -30,18 +30,7 @@ def registration(info):
                     'customer_address_doc': customer_address_doc_name
                 }
 
-    customer_addres_doc_name_from_serial_no_query = """select so.customer_address from `tabDelivery Note Item`  as dni inner join `tabDelivery Note` as dn on dn.name = dni.parent inner join `tabSales Order` as so on so.name = dni.against_sales_order where dni.serial_no like "%%{0}%%" and dn.status = "Completed" order by dni.creation DESC LIMIT 1"""
-    check_for_upper_case = customer_addres_doc_name_from_serial_no_query.format(info['serial_no'].strip())
-
-    customer_address_doc_name_dict = frappe.db.sql(customer_addres_doc_name_from_serial_no_query, as_dict=1)
-    customer_address_doc_name = customer_address_doc_name_dict[0]['customer_address']
-    if customer_address_doc_name:
-        return {
-            'status':True,
-            'customer_address_doc':customer_address_doc_name
-        }
-    check_for_upper_case = customer_addres_doc_name_from_serial_no_query.format(info['serial_no'].strip().lower())
-
+    customer_addres_doc_name_from_serial_no_query = """select so.customer_address from `tabDelivery Note Item`  as dni inner join `tabDelivery Note` as dn on dn.name = dni.parent inner join `tabSales Order` as so on so.name = dni.against_sales_order where dni.serial_no like "%%{0}%%" and dn.status = "Completed" order by dni.creation DESC LIMIT 1""".format(info['serial_no'].strip())
     customer_address_doc_name_dict = frappe.db.sql(customer_addres_doc_name_from_serial_no_query, as_dict=1)
     customer_address_doc_name = customer_address_doc_name_dict[0]['customer_address']
     if customer_address_doc_name:
