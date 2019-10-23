647e1bf41705dc269b786de35b03233f1cc6a11f
khopadi <it@usedyetnew.com>
2019-10-23 17:47:15 +0530

    report stock balance by item group

---
+++ erpnext_uyn_customizations/erpnext_uyn_customizations/report/stock_balance_by_item_group/stock_balance_by_item_group.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Stock Balance by Item Group"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname": "item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Item Group"
+		}
+	]
+}
